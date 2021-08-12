be04ccb5a847a3d71b614cbdcef71cf28ec619fe
Charlie <me@charliemoomoo.cloudns.asia>
2021-08-12 14:18:33 +0800

    Update fade.js

--- fade.js
+++ fade.js
@@ -1,7 +1,7 @@
 function fadein(){
   var i,alpha,oDiv
-  for(i=0;i<100;i+-2){
-oDiv = document.getElementById('div1'); 
+  for(i=100;i<-1;i++2){
+oDiv = document.getElementById('projects'); 
 oDiv.style.filter = 'alpha(opacity:'+i+')'; 
 oDiv.style.opacity = i / 100;
 }
